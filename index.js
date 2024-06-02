19a2edd58e7ae97224c31967f323743fd3b84fb7
Saurav Nautiyal <sauravnyt34@gmail.com>
2024-06-02 17:30:59 +0530

    updated bcrypt js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const dbConnect = require("./connection/connection");
 const User = require("./models/user");
 
